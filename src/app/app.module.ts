a987368a3bf07b12a093f7bb0e15dd499f0c4c20
Ojas Shirekar <oshireka@amgen.com>
2018-07-17 20:11:51 +0530

    add the hieararchy module - now fully functional
    modified the styles for the same.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,34 +1,30 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { HttpModule } from '@angular/http';
-import { FlashMessagesModule } from 'angular2-flash-messages';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {HttpModule} from '@angular/http';
+import {FlashMessagesModule} from 'angular2-flash-messages';
 
-import { AppComponent } from './app.component';
-import { SearchComponent } from './search/search.component';
+import {AppComponent} from './app.component';
+import {SearchComponent} from './search/search.component';
 
-import { SearchService } from './search/search.service';
-import { SearchResultService } from './search-result/search-result.service';
-import { DescriptionService } from './description/description.service';
-import { HierarchyService } from './search-result/hierarchy.service';
+import {SearchService} from './search/search.service';
+import {SearchResultService} from './search-result/search-result.service';
+import {DescriptionService} from './description/description.service';
+import {HierarchyService} from './search-result/hierarchy.service';
+import {SearchResultComponent} from './search-result/search-result.component';
 
-
-import { ReactiveFormsModule } from '@angular/forms';
-import { SearchResultComponent } from './search-result/search-result.component';
-
-import { RouterModule, Routes } from '@angular/router';
-import {shallowEqualArrays} from '@angular/router/src/utils/collection';
-import { ExpandableListModule } from 'angular2-expandable-list';
-import { DescriptionComponent } from './description/description.component';
-import { NavbarComponent } from './navbar/navbar.component';
-import { CheckoutComponent } from './checkout/checkout.component';
+import {RouterModule, Routes} from '@angular/router';
+import {ExpandableListModule} from 'angular2-expandable-list';
+import {DescriptionComponent} from './description/description.component';
+import {NavbarComponent} from './navbar/navbar.component';
+import {CheckoutComponent} from './checkout/checkout.component';
 
 
 const appRoutes: Routes = [
-  {path:'', component: SearchComponent},
-  {path:'search-result', component: SearchResultComponent},
-  {path:'description', component: DescriptionComponent},
-  {path:'checkout', component: CheckoutComponent}
+  {path: '', component: SearchComponent},
+  {path: 'search-result', component: SearchResultComponent},
+  {path: 'description', component: DescriptionComponent},
+  {path: 'checkout', component: CheckoutComponent}
 ];
 
 @NgModule({
@@ -52,4 +48,5 @@ const appRoutes: Routes = [
   providers: [SearchService, SearchResultService, DescriptionService, HierarchyService],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
--- src/app/search-result/search-result.component.ts
+++ src/app/search-result/search-result.component.ts
@@ -1,11 +1,11 @@
-import {Component, Inject, Injectable, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {Component, Inject, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 import {SearchResultService} from './search-result.service';
 import {HierarchyService} from './hierarchy.service';
-import {Router} from '@angular/router';
 import {DOCUMENT} from '@angular/platform-browser';
 
 import 'rxjs/add/operator/switchMap'
+
 @Component({
   selector: 'app-search-result',
   templateUrl: './search-result.component.html',
@@ -18,17 +18,30 @@ export class SearchResultComponent implements OnInit {
   searchResult: string;
   results: any[] = [];
   results_arrays: any[] = [];
-
+  HierarchyHtml;
   counter: number;
-  data_list: any[] =[];
-  hierarchy_list: any[] =[["Parent Concept"], ["Child Concept", "Parallel Child Concept"], ["Final Concept"]];
-
+  data_list: any[] = [];
+  hierarchy_list: any[] = [["Parent Concept"], ["Child Concept", "Parallel Child Concept"], ["Final Concept"]];
+  private visited: Object = {};
+  private parentMap: Object = {};
 
   constructor(@Inject(DOCUMENT) private document: any,
               private activateRoute: ActivatedRoute,
               private _api_service: SearchResultService,
               private _hierarchy_service: HierarchyService,
               private router: Router) {
+  }
+
+  private renderTree(graph: Object, root: string) {
+    let html = "<ul>";
+    for (let child of graph[root]) {
+      console.log(`### ${child}`);
+      html += "<li class='hierarchy-item'>" + child;
+      html += this.renderTree(graph, child);
+      html += "</li>";
+    }
+    html += "</ul>";
+    return html;
   }
 
   ngOnInit() {
@@ -51,14 +64,14 @@ export class SearchResultComponent implements OnInit {
           console.log("Service gave a success response!");
           console.log(Object.keys(result["result"]).length);
           console.log(Object.keys(this.datasource_concept_list).length);
-          for(let i =0; i<Object.keys(this.datasource_concept_list).length; i++){
+          for (let i = 0; i < Object.keys(this.datasource_concept_list).length; i++) {
             this.results[this.datasource_concept_list[i]] = result.result[this.datasource_concept_list[i]];
           }
           console.log("The value in this.results -");
           console.log(this.results);
           //console.log(this.results["Equipment"]["fact_table_list"]);
           console.log(this.datasource_concept_list);
-          for(result in this.results){
+          for (result in this.results) {
             console.log(result);
             this.results_arrays.push([result, this.results[result]['fact_table_list'], 'fact_table']);
             this.results_arrays.push([result, this.results[result]['document_list'], 'document']);
@@ -87,32 +100,34 @@ export class SearchResultComponent implements OnInit {
     console.log(search_term);
     this._hierarchy_service.fetchHierarchy(search_term).subscribe(result => {
       console.log("Result from the hierarchy service - ");
-      console.log(result);
-      for(let single_item in result.result){
-        console.log("Single Item");
-        console.log(single_item);
-      //   for(let single_hierarchy in result.result[single_item]){
-      //     console.log("Single Hierarchy");
-      //     console.log(single_hierarchy);
-      //   }
-
+      let res = result.result;
+      console.log(res);
+      let sub_graph = res.hierarchy;
+      let roots = res.roots;
+      console.log(`++++ ${roots}`);
+      for (let node in sub_graph) {
+        this.visited[node] = false;
+        this.parentMap[node] = [];
       }
+      // this.dfs(sub_graph, "Process development");
+      this.HierarchyHtml = roots.map(x => `<ul> <li class="hierarchy-item"> ${x} </li>` + this.renderTree(sub_graph, x));
+      console.log(this.HierarchyHtml);
     });
   }
 
-  selectedData(data, type_of_data){
+  selectedData(data, type_of_data) {
     console.log("Selected data is -");
     console.log(data);
     console.log(type_of_data);
-    if(type_of_data === "document"){
+    if (type_of_data === "document") {
       console.log("The type of data selected is a document!");
       this.document.location.href = data[0];
-    }else if(type_of_data === "public_ontology"){
+    } else if (type_of_data === "public_ontology") {
       console.log("The type of data selected is a public ontology");
       this.document.location.href = data[0];
-    }else{
+    } else {
       let related_dataset_payload = {
-        "selected_data_uri" : data[0],
+        "selected_data_uri": data[0],
         "selected_data_name": data[1],
         "short_description": data[2]
       };
